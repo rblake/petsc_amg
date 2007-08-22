f89b775731885903356245a06971165259ea5851
lukeo <lukeo@c7f3fe13-8c30-0410-9127-9a9c42fe009c>
2007-08-22 19:26:04 +0000

    0 level MG now works :)   In fact there's an ex in tutorials just about like this one
    git-svn-id: file:///home/rblake/svn/pamg/trunk@18 c7f3fe13-8c30-0410-9127-9a9c42fe009c

--- petsc_amg/luke.c
+++ petsc_amg/luke.c
@@ -1,8 +1,11 @@
 /* 
  ------
- log:
-  lukeo:  Tue Aug 21 10:31:15 CDT 2007
+ log (use :r!date in vim):
+  lukeo: Wed Aug 22 09:15:07 CDT 2007
+      - started working on the shell context for AMG
+  lukeo: Tue Aug 21 10:31:15 CDT 2007
       - touched
+      - copied some structure from tutorial/ex*.c
  ------
  
  use
@@ -11,6 +14,7 @@
  $PETSC_DIR/bin/petscmpirun -np 8 ./luke args
  
  */
+#include "petscda.h"
 #include "petscksp.h"
 
 static char help[] = "Easy MG\n\
@@ -18,41 +22,56 @@ static char help[] = "Easy MG\n\
                         -kx : integer for sin wave exact soln in x-dir\n\
                         -ky : integer for sin wave exact soln in y-dir\n\
                         -nx : mesh points in x-dir\n\
-                        -ny : mesh points in y-dir\n\";
+                        -ny : mesh points in y-dir\n";
 
-/* TODO: move SCAMG stuff to scamg.h */
+/* TODO: move ScAMG stuff to scamg.h */
 
 /* context for MG preconditioner (0 level) */
 typedef struct {
-  Vec diag;
-} SCAMG;
+  Vec diaginv;
+} ScAMG;
 
-/* declarations for SCAMG routines */
-extern PetscErrorCode SCAMGCreate(SCAMG**);
-extern PetscErrorCode SCAMGSetUp(SCAMG*,Mat,Vec);
-extern PetscErrorCode SCAMGApply(void*,Vec x,Vec y);
-extern PetscErrorCode SCAMGDestroy(SCAMG*);
+/* declarations for ScAMG routines */
+extern PetscErrorCode ScAMGCreate(ScAMG**);
+extern PetscErrorCode ScAMGSetUp(ScAMG*,Mat,Vec);
+extern PetscErrorCode ScAMGApply(void*,Vec x,Vec y);
+extern PetscErrorCode ScAMGDestroy(ScAMG*);
 
 #undef __FUNCT__  
 #define __FUNCT__ "main"
-int main(int argc,char **args)
-{
-  Vec            x,b,xex;   /* approx solution, RHS, exact solution */
+int 
+main(int argc,char **args) {
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     variables
+  */
+  Vec            u,b,uex,uerr;   /* approx solution, RHS, exact solution, error */
   Mat            A;         /* linear system matrix */
   KSP            ksp;       /* linear solver context */
   PC             pc;        /* preconditioner context */
   PetscReal      norm;      /* norm of solution error */
-  SCAMG          *SCAMGsh;  /* MG context */
+  ScAMG          *ScAMGsh;  /* MG context */
   PetscScalar    v,one = 1.0,neg_one = -1.0;
   PetscInt       i,j,Ii,J,Istart,Iend,nx = 10,ny = 8,its,kx=1,ky=1;
   PetscErrorCode ierr;
-  PetscTruth     use_SCAMG;
+  PetscTruth     use_ScAMG;
+  PetscReal      ksp_tol=1.e-7;
+  PetscReal      hx,hy;
+  PetscReal      x,y,val;
+  PetscLogDouble v1,v2,elapsed_time;
 
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     initialization
+  */
   PetscInitialize(&argc,&args,(char *)0,help);
   ierr = PetscOptionsGetInt(PETSC_NULL,"-nx",&nx,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetInt(PETSC_NULL,"-ny",&ny,PETSC_NULL);CHKERRQ(ierr);
 
-  /* Generate Ax=b for a FD 2D Laplacian with Dirichlet BC on [0,1]^2*/
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     problem setup
+  */
+  /* Generate Au=b for a FD 2D Laplacian with Dirichlet BC on [0,1]^2*/
+  hx = 1.0/(nx-1);
+  hy = 1.0/(ny-1);
   ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
   ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,nx*ny,nx*ny);CHKERRQ(ierr);
   ierr = MatSetFromOptions(A);CHKERRQ(ierr);
@@ -85,249 +104,175 @@ int main(int argc,char **args)
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
 
-  ierr = VecCreate(PETSC_COMM_WORLD,&xex);CHKERRQ(ierr);
-  ierr = VecSetSizes(xex,PETSC_DECIDE,nx*ny);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(xex);CHKERRQ(ierr);
-  ierr = VecDuplicate(xex,&b);CHKERRQ(ierr); 
-  ierr = VecDuplicate(xex,&x);CHKERRQ(ierr);
+  ierr = VecCreate(PETSC_COMM_WORLD,&uex);CHKERRQ(ierr);
+  ierr = VecSetSizes(uex,PETSC_DECIDE,nx*ny);CHKERRQ(ierr);
+  ierr = VecSetFromOptions(uex);CHKERRQ(ierr);
+  ierr = VecDuplicate(uex,&b);CHKERRQ(ierr); 
+  ierr = VecDuplicate(uex,&u);CHKERRQ(ierr);
+  ierr = VecDuplicate(uex,&uerr);CHKERRQ(ierr);
 
-  /* xex is sin(kx*pi*x)*sin(ky*pi*y) */
-  ierr = VecGetOwnershipRange(xex,&Istart,&Iend);CHKERRQ(ierr);
+  /* uex is sin(kx*pi*x)*sin(ky*pi*y) */
+  ierr = VecGetOwnershipRange(uex,&Istart,&Iend);CHKERRQ(ierr);
   for (i=Istart; i<Iend; i++) {
-     x = h*(i % (nx+1)); y = h*(i/(nx+1)); 
-     val = sin(kx*3.14159*x)*sin(ky*3.14159*y);
-     /* TODO: better PI */
-     ierr = VecSetValues(xex,1,&i,&val,INSERT_VALUES);CHKERRQ(ierr);
+    x = hx*(i % (nx+1)); 
+    y = hy*(i/(nx+1)); 
+     val = sin(kx*PETSC_PI*x)*sin(ky*PETSC_PI*y);
+     ierr = VecSetValues(uex,1,&i,&val,INSERT_VALUES);CHKERRQ(ierr);
   }
-  ierr = VecAssemblyBegin(xex);CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(xex);CHKERRQ(ierr);
-  ierr = VecSet(x,one);CHKERRQ(ierr);
-  ierr = MatMult(A,xex,b);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(uex);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(uex);CHKERRQ(ierr);
+  ierr = VecSet(u,one);CHKERRQ(ierr);
+  ierr = VecSet(uerr,0.0);CHKERRQ(ierr);
+  /* ierr = VecSet(uex,one);CHKERRQ(ierr); */
+  ierr = MatMult(A,uex,b);CHKERRQ(ierr);
 
-  /*TODO: got to here */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-                Create the linear solver and set various options
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-  /* 
-     Create linear solver context
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    solver setup
   */
+  /* Create linear solver context */
   ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
-
-  /* 
-     Set operators. Here the matrix that defines the linear system
-     also serves as the preconditioning matrix.
-  */
   ierr = KSPSetOperators(ksp,A,A,DIFFERENT_NONZERO_PATTERN);CHKERRQ(ierr);
-
-  /* 
-     Set linear solver defaults for this problem (optional).
-     - By extracting the KSP and PC contexts from the KSP context,
-       we can then directly call any KSP and PC routines
-       to set various options.
-  */
   ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
-  ierr = KSPSetTolerances(ksp,1.e-7,PETSC_DEFAULT,PETSC_DEFAULT,
-         PETSC_DEFAULT);CHKERRQ(ierr);
+  ierr = KSPSetTolerances(ksp,ksp_tol,PETSC_DEFAULT,PETSC_DEFAULT, PETSC_DEFAULT);CHKERRQ(ierr);
 
-  /*
-     Set a user-defined "shell" preconditioner if desired
-  */
-  ierr = PetscOptionsHasName(PETSC_NULL,"-use_SCAMG",&use_SCAMG);CHKERRQ(ierr);
-  if (use_SCAMG) {
-    /* (Required) Indicate to PETSc that we're using a "shell" preconditioner */
+  /* use ScAMG or not */
+  ierr = PetscOptionsHasName(PETSC_NULL,"-use_ScAMG",&use_ScAMG);CHKERRQ(ierr);
+  if (use_ScAMG) {
     ierr = PCSetType(pc,PCSHELL);CHKERRQ(ierr);
-
-    /* (Optional) Create a context for the user-defined preconditioner; this
-       context can be used to contain any application-specific data. */
-    ierr = SCAMGCreate(&SCAMGsh);CHKERRQ(ierr);
-
-    /* (Required) Set the user-defined routine for applying the preconditioner */
-    ierr = PCShellSetApply(pc,SCAMGApply);CHKERRQ(ierr);
-    ierr = PCShellSetContext(pc,SCAMGsh);CHKERRQ(ierr);
-
-    /* (Optional) Set a name for the preconditioner, used for PCView() */
-    ierr = PCShellSetName(pc,"MyPreconditioner");CHKERRQ(ierr);
-
-    /* (Optional) Do any setup required for the preconditioner */
-    ierr = SCAMGSetUp(SCAMGsh,A,x);CHKERRQ(ierr);
+    ierr = ScAMGCreate(&ScAMGsh);CHKERRQ(ierr);
+    ierr = PCShellSetApply(pc,ScAMGApply);CHKERRQ(ierr);
+    ierr = PCShellSetContext(pc,ScAMGsh);CHKERRQ(ierr);
+    ierr = PCShellSetName(pc,"ScAMG");CHKERRQ(ierr);
+    ierr = ScAMGSetUp(ScAMGsh,A,u);CHKERRQ(ierr);
 
   } else {
     ierr = PCSetType(pc,PCJACOBI);CHKERRQ(ierr);
   }
 
-  /* 
-    Set runtime options, e.g.,
-        -ksp_type <type> -pc_type <type> -ksp_monitor -ksp_rtol <rtol>
-    These options will override those specified above as long as
-    KSPSetFromOptions() is called _after_ any other customization
-    routines.
-  */
+  /* override with runtime args */
   ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
 
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-                      Solve the linear system
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-  ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-                      Check solution and clean up
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-  /* 
-     Check the error
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    solve
   */
-  ierr = VecAXPY(x,neg_one,u);CHKERRQ(ierr);
-  ierr = VecNorm(x,NORM_2,&norm);CHKERRQ(ierr);
-  ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"Norm of error %A iterations %D\n",norm,its);CHKERRQ(ierr);
+  ierr = PetscGetTime(&v1);CHKERRQ(ierr);
+  ierr = KSPSolve(ksp,b,u);CHKERRQ(ierr);
+  ierr = PetscGetTime(&v2);CHKERRQ(ierr);
+  elapsed_time = v2 - v1;
 
-  /* 
-     Free work space.  All PETSc objects should be destroyed when they
-     are no longer needed.
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    post-process
+  */
+  /* Check the error uerr=uex-u */
+  ierr = VecWAXPY(uerr,neg_one,u,uex);CHKERRQ(ierr);
+  ierr = VecNorm(uerr,NORM_2,&norm);CHKERRQ(ierr);
+  ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"time: %A  ||e|| = %G after %D iterations\n",elapsed_time,norm,its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"%A\n",elapsed_time);CHKERRQ(ierr);
+
+  /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    clean up
   */
   ierr = KSPDestroy(ksp);CHKERRQ(ierr);
-  ierr = VecDestroy(u);CHKERRQ(ierr);  ierr = VecDestroy(x);CHKERRQ(ierr);
-  ierr = VecDestroy(b);CHKERRQ(ierr);  ierr = MatDestroy(A);CHKERRQ(ierr);
+  ierr = VecDestroy(u);CHKERRQ(ierr);  
+  ierr = VecDestroy(uex);CHKERRQ(ierr);
+  ierr = VecDestroy(uerr);CHKERRQ(ierr);
+  ierr = VecDestroy(b);CHKERRQ(ierr);  
+  ierr = MatDestroy(A);CHKERRQ(ierr);
 
-  if (use_SCAMG) {
-    ierr = SCAMGDestroy(SCAMGsh);CHKERRQ(ierr);
+  if (use_ScAMG) {
+    ierr = ScAMGDestroy(ScAMGsh);CHKERRQ(ierr);
   }
 
   ierr = PetscFinalize();CHKERRQ(ierr);
   return 0;
-
 }
 
-/***********************************************************************/
-/*          Routines for a user-defined SCAMG preconditioner           */
-/***********************************************************************/
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ Routines for ScAMG
+*/
 
-#undef __FUNCT__  
-#define __FUNCT__ "SCAMGCreate"
 /*
-   SCAMGCreate - This routine creates a user-defined
-   preconditioner context.
+   ScAMGCreate - create ScAMG context
 
    Output Parameter:
-.  SCAMGsh - user-defined preconditioner context
+.  ScAMGsh - ScAMG context
 */
-PetscErrorCode SCAMGCreate(SCAMG **SCAMGsh)
-{
-  SCAMG  *newctx;
+#undef __FUNCT__  
+#define __FUNCT__ "ScAMGCreate"
+PetscErrorCode 
+ScAMGCreate(ScAMG **ScAMGsh) {
+  ScAMG  *context;
   PetscErrorCode ierr;
 
-  ierr         = PetscNew(SCAMG,&newctx);CHKERRQ(ierr);
-  newctx->diag = 0;
-  *SCAMGsh       = newctx;
+  ierr         = PetscNew(ScAMG,&context);CHKERRQ(ierr);
+  context->diaginv = 0;
+  *ScAMGsh       = context;
   return 0;
 }
-/* ------------------------------------------------------------------- */
-#undef __FUNCT__  
-#define __FUNCT__ "SCAMGSetUp"
+
 /*
-   SCAMGSetUp - This routine sets up a user-defined
-   preconditioner context.  
+   ScAMGSetUp - set up ScAMG context  
 
    Input Parameters:
-.  SCAMGsh - user-defined preconditioner context
-.  pmat  - preconditioner matrix
+.  ScAMGsh - ScAMG context
+.  M  - preconditioner matrix (usually A; set in KSP setup)
 .  x     - vector
 
    Output Parameter:
-.  SCAMGsh - fully set up user-defined preconditioner context
-
-   Notes:
-   In this example, we define the shell preconditioner to be Jacobi's
-   method.  Thus, here we create a work vector for storing the reciprocal
-   of the diagonal of the preconditioner matrix; this vector is then
-   used within the routine SCAMGApply().
+.  ScAMGsh - initialized ScAMG context
 */
-PetscErrorCode SCAMGSetUp(SCAMG *shell,Mat pmat,Vec x)
-{
-  Vec            diag;
+#undef __FUNCT__  
+#define __FUNCT__ "ScAMGSetUp"
+PetscErrorCode 
+ScAMGSetUp(ScAMG *ScAMGsh,Mat M,Vec x) {
+  Vec            diaginv;
   PetscErrorCode ierr;
 
-  ierr = VecDuplicate(x,&diag);CHKERRQ(ierr);
-  ierr = MatGetDiagonal(pmat,diag);CHKERRQ(ierr);
-  ierr = VecReciprocal(diag);CHKERRQ(ierr);
-  SCAMGsh>diag = diag;
+  ierr = VecDuplicate(x,&diaginv);CHKERRQ(ierr);
+  ierr = MatGetDiagonal(M,diaginv);CHKERRQ(ierr);
+  ierr = VecReciprocal(diaginv);CHKERRQ(ierr);
+  ScAMGsh->diaginv = diaginv;
 
   return 0;
 }
-/* ------------------------------------------------------------------- */
-#undef __FUNCT__  
-#define __FUNCT__ "SCAMGApply"
+
 /*
-   SCAMGApply - This routine demonstrates the use of a
-   user-provided preconditioner.
+   ScAMGApply - Run ScAMG
 
    Input Parameters:
-.  ctx - optional user-defined context, as set by PCShellSetContext()
+.  context - generic context, as set by PCShellSetContext()
 .  x - input vector
 
    Output Parameter:
 .  y - preconditioned vector
-
-   Notes:
-   Note that the PCSHELL preconditioner passes a void pointer as the
-   first input argument.  This can be cast to be the whatever the user
-   has set (via PCSetShellApply()) the application-defined context to be.
-
-   This code implements the Jacobi preconditioner, merely as an
-   example of working with a PCSHELL.  Note that the Jacobi method
-   is already provided within PETSc.
 */
-PetscErrorCode SCAMGApply(void *ctx,Vec x,Vec y)
-{
-  SCAMG   *SCAMGsh = (SCAMG*)ctx;
+#undef __FUNCT__  
+#define __FUNCT__ "ScAMGApply"
+PetscErrorCode
+ScAMGApply(void *context,Vec x,Vec y) {
+  ScAMG   *ScAMGsh = (ScAMG*)context;
   PetscErrorCode  ierr;
 
-  ierr = VecPointwiseMult(y,x,SCAMGsh>diag);CHKERRQ(ierr);
+  ierr = VecPointwiseMult(y,x,ScAMGsh->diaginv);CHKERRQ(ierr);
 
   return 0;
 }
-/* ------------------------------------------------------------------- */
-#undef __FUNCT__  
-#define __FUNCT__ "SCAMGDestroy"
+
 /*
-   SCAMGDestroy - This routine destroys a user-defined
-   preconditioner context.
+   ScAMGDestroy - destroy the ScAMG context
 
    Input Parameter:
-.  SCAMGsh - user-defined preconditioner context
+.  ScAMGsh - ScAMG context
 */
-PetscErrorCode SCAMGDestroy(SCAMG *SCAMGsh)
-{
+#undef __FUNCT__  
+#define __FUNCT__ "ScAMGDestroy"
+PetscErrorCode
+ScAMGDestroy(ScAMG *ScAMGsh) {
   PetscErrorCode ierr;
 
-  ierr = VecDestroy(SCAMGsh>diag);CHKERRQ(ierr);
-  ierr = PetscFree(SCAMGsh);CHKERRQ(ierr);
+  ierr = VecDestroy(ScAMGsh->diaginv);CHKERRQ(ierr);
+  ierr = PetscFree(ScAMGsh);CHKERRQ(ierr);
 
   return 0;
 }
-
-
